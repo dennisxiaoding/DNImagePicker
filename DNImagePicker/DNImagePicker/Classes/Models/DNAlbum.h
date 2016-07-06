d41b72023304ea792cc71299bd56664eed52f7fe
dingxiao01 <dingxiao01@baidu.com>
2016-07-06 19:53:02 +0800

    ci
    ci

---
+++ DNImagePicker/DNImagePicker/Classes/Models/DNAlbum.h
@@ -0,0 +1,13 @@
+//
+//  DNAlbum.h
+//  DNImagePicker
+//
+//  Created by Ding Xiao on 16/7/6.
+//  Copyright © 2016年 Dennis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DNAlbum : NSObject
+
+@end
---
+++ DNImagePicker/DNImagePicker/Classes/Models/DNAlbum.m
@@ -0,0 +1,13 @@
+//
+//  DNAlbum.m
+//  DNImagePicker
+//
+//  Created by Ding Xiao on 16/7/6.
+//  Copyright © 2016年 Dennis. All rights reserved.
+//
+
+#import "DNAlbum.h"
+
+@implementation DNAlbum
+
+@end
